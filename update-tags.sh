1eb11146e6c23038e1de06c01c362a62c512750f
Shiv Kokroo <kokroo@users.noreply.github.com>
2022-08-03 01:32:55 +0200

    release: v22.03_b202208010100-n =ramips-mt7621
    Signed-off-by: Shiv Kokroo <kokroo@users.noreply.github.com>

--- update-tags.sh
+++ update-tags.sh
@@ -8,12 +8,12 @@ cat target.list | while read target; do
 	test -n "$1" && {
 		echo $target | grep -q "$1" || continue
 	}
-	TAG="`cat release.tag`""`echo -n $target | sed 's/TARGET//'`"
+	TAG="`cat release.tag`""`echo -n $target | gsed 's/TARGET//'`"
 	echo push $TAG
 	sleep 2
 
-	sed -i "s/name: x-wrt-.*=.*/name: x-wrt-$TAG/g" .github/workflows/main.yml && \
-		sed -i "s/TARGET=.* sh /$target sh /" .github/workflows/main.yml && \
+	gsed -i "s/name: x-wrt-.*=.*/name: x-wrt-$TAG/g" .github/workflows/main.yml && \
+		gsed -i "s/TARGET=.* sh /$target sh /" .github/workflows/main.yml && \
 		git commit --signoff -am "release: $TAG" && \
 		git push origin master || exit 1
 done
