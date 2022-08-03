f6ee5e0941ae2f03168dba2fce7e58de7cecef36
Shiv Kokroo <kokroo@users.noreply.github.com>
2022-08-03 15:48:53 +0200

    release: 22.03_b202208010100-n =ramips-mt7621
    Signed-off-by: Shiv Kokroo <kokroo@users.noreply.github.com>

--- update-tags.sh
+++ update-tags.sh
@@ -15,5 +15,10 @@ cat target.list | while read target; do
 	gsed -i "s/name: x-wrt-.*=.*/name: x-wrt-$TAG/g" .github/workflows/main.yml && \
 		gsed -i "s/TARGET=.* sh /$target sh /" .github/workflows/main.yml && \
 		git commit --signoff -am "release: $TAG" && \
-		git push origin master || exit 1
+		git push -f --tags origin master || exit 1
 done
+
+# TAG="v`cat release.tag`"
+# git commit --signoff -am "release: $TAG" && \
+# 		git tag $TAG && \
+# 		git push -f --tags origin parallel-jobs 
