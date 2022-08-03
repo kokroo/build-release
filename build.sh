6eb3a0aae9b43db81544ddf5eb3408d702efd8cb
Shiv Kokroo <kokroo@users.noreply.github.com>
2022-08-03 06:28:43 +0200

    f

--- build.sh
+++ build.sh
@@ -19,6 +19,11 @@ sleep 10
 
 mkdir .build_x
 echo CONFIG_VERSION_NUMBER=\"`cat release.tag`\" >.build_x/env
+sed -i 's,cp feeds/x/rom/lede/$cfg .config,cp $cfg .config,g' ./feeds/x/rom/lede/make.sh
+echo 'Printing modded make.sh file below'
+echo `cat ./feeds/x/rom/lede/make.sh`
+echo 'Present working dir'
+echo "$PWD"
 ./feeds/x/rom/lede/make.sh make -j$1 && sh upload.sh
 
 _EXIT=$?
