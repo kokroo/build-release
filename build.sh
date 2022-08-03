d14740db6079467adb81320bd8086ac1c711308f
Shiv Kokroo <kokroo@users.noreply.github.com>
2022-08-03 06:28:12 +0200

    f

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 
-TARGET=${TARGET-x86_64}
+BTARGET=${BTARGET-x86_64}
 
 CFGS=`for t in $BTARGET; do
 	cat ./feeds/x/rom/lede/cfg.list | grep "^config.$t$"
 done`
 
 export CFGS="`echo $CFGS`"
-export WORKFLOW="1"
+export WORKFLOW="0"
 
 echo starting build.
 echo starting build..
@@ -19,12 +19,6 @@ sleep 10
 
 mkdir .build_x
 echo CONFIG_VERSION_NUMBER=\"`cat release.tag`\" >.build_x/env
-sed -i 's,cp feeds/x/rom/lede/$cfg .config,cp $cfg .config,g' ./feeds/x/rom/lede/make.sh
-echo 'Printing modded make.sh file below'
-echo `cat ./feeds/x/rom/lede/make.sh`
-echo 'Present working dir'
-echo "$PWD"
-
 ./feeds/x/rom/lede/make.sh make -j$1 && sh upload.sh
 
 _EXIT=$?
